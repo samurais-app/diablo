d1625bf86d2e3b8e059b3414700d044300f915ae
刘伟 <lemonpaimc@126.com>
2023-03-11 11:24:25 +0800

    wip: 组件库

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -13,7 +13,7 @@ export const onRenderBody = (body) => {
   body.setHeadComponents([
     <meta charSet="utf-8" key="utf-8" />,
     <meta httpEquiv="x-ua-compatible" content="ie=edge" key="edge" />,
-    <meta name="viewport" content="width=device-width,initial-scale=1" key="view" />,
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no" key="view" />,
   ]);
 };
 
